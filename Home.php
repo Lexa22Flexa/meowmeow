235e5d1e4efcf557528a2f3d70505cd4fc1c8ae4
Lexa22Flexa <165304856+Lexa22Flexa@users.noreply.github.com>
2025-06-12 05:42:23 +0200

    Dodání selectu

--- Home.php
+++ Home.php
@@ -1,5 +1,6 @@
 <?php
-
+// $stages = $this->zavodyYear->select("race_year.*, Count(*) as pocetZavodu")->join("stage", "stage.id_race_year = race_year.id", "inner")->where("id_race", $idRace)->groupBy("race_year.id")->findAll();
+//      $data["stages"] = $stages;
 // v konstruktoru: $this->load->helper("fileupload");
 public function submitFile() {
     echo $image_new=fileuploadCI($nazevObrazku, "obrazky");
